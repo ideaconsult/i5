518e3383b717952f842e1bb3112ab9d746693014
ngn <jeliazkova.nina@gmail.com>
2013-08-02 15:01:39 +0300

    Align the ReferenceSubstance parser with ambit2.core.io.I5ReaderSimple

--- iuclid_5_0-io/src/main/java/net/idea/i5/_0/ambit2/I5AmbitProcessor.java
+++ iuclid_5_0-io/src/main/java/net/idea/i5/_0/ambit2/I5AmbitProcessor.java
@@ -1,14 +1,22 @@
 package net.idea.i5._0.ambit2;
 
+import ambit2.base.data.LiteratureEntry;
 import ambit2.base.data.Property;
 import ambit2.base.data.SubstanceRecord;
 import ambit2.base.exceptions.AmbitException;
 import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.processors.CASProcessor;
 import ambit2.base.processors.DefaultAmbitProcessor;
+import ambit2.core.config.AmbitCONSTANTS;
+import ambit2.core.io.ECHAPreregistrationListReader;
+import ambit2.core.io.I5ReaderSimple;
 import eu.europa.echa.schemas.iuclid5._20070330.referencesubstance.ReferenceSubstance;
+import eu.europa.echa.schemas.iuclid5._20070330.referencesubstance.ReferenceSubstance.ReferenceSubstanceInformation.CasInformation;
+import eu.europa.echa.schemas.iuclid5._20070330.referencesubstance.ReferenceSubstance.ReferenceSubstanceInformation.Synonyms;
 
 public class I5AmbitProcessor<Target> extends DefaultAmbitProcessor<Target, IStructureRecord> {
 	protected SubstanceRecord record = new SubstanceRecord();
+	protected CASProcessor casProcessor = new CASProcessor();
 	/**
 	 * 
 	 */
@@ -23,16 +31,47 @@ public class I5AmbitProcessor<Target> extends DefaultAmbitProcessor<Target, IStr
 	protected IStructureRecord transform2record(ReferenceSubstance unmarshalled) {
 		record.clear();
 		if (unmarshalled.getDocumentReferencePK()!=null)
-			record.setProperty(Property.getI5UUIDInstance(),unmarshalled.getDocumentReferencePK());
+			setUUID(record,unmarshalled.getDocumentReferencePK());
 		if (unmarshalled.getName()!=null)
 			record.setProperty(Property.getNameInstance(),unmarshalled.getName());
+		
+		if (unmarshalled.getEcSubstanceInventoryEntryRef().getNumber()!=null)
+			record.setProperty(I5ReaderSimple.ecProperty,unmarshalled.getEcSubstanceInventoryEntryRef().getNumber());
+		
 		if (unmarshalled.getReferenceSubstanceStructure()!=null) {
 			record.setInchi(unmarshalled.getReferenceSubstanceStructure().getInChI());
 			record.setSmiles(unmarshalled.getReferenceSubstanceStructure().getSmilesNotation());
 			//record.setFormula(unmarshalled.getReferenceSubstanceStructure().getStructureFormula().);
 		}
+		CasInformation cas = unmarshalled.getReferenceSubstanceInformation().getCasInformation();
+		if (cas!=null) {
+			try {
+				record.setProperty(I5ReaderSimple.casProperty,casProcessor.process(cas.getCasNumber()));
+			} catch (Exception x) {
+    			record.setProperty(I5ReaderSimple.casProperty,cas.getCasNumber());
+			}
+		}
+		if (unmarshalled.getReferenceSubstanceInformation().getIupacName()!=null)
+			record.setProperty(Property.getNameInstance(),unmarshalled.getReferenceSubstanceInformation().getIupacName());
+		
+		Synonyms synonyms = unmarshalled.getReferenceSubstanceInformation().getSynonyms();
+		if (synonyms!=null)
+		for (int i=0;i < synonyms.getSynonym().size();i++)
+			record.setProperty(
+					Property.getInstance(AmbitCONSTANTS.NAMES,
+					LiteratureEntry.getInstance(String.format("%s %s#%d",I5ReaderSimple.I5_REFERENCE,
+							ECHAPreregistrationListReader.echa_tags.SYNONYM.toString(),i+1, 
+							I5ReaderSimple.I5_URL),I5ReaderSimple.I5_URL))
+					,synonyms.getSynonym().get(i));		
 			
 		return record;
 	}
-	
+
+	protected void setUUID(IStructureRecord record,String value) {
+		int slashpos = value.indexOf("/");
+		if (slashpos>0)
+			record.setProperty(Property.getI5UUIDInstance(),value.substring(0,slashpos));
+		else
+			record.setProperty(Property.getI5UUIDInstance(),value);
+	}
 }
