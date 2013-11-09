105d977c527e8015f8ca658383215dc580665f78
ngn <jeliazkova.nina@gmail.com>
2013-11-09 13:07:12 +0200

    lot of species added

--- iuclid_5_5-io/src/main/java/net/idea/i5/_5/ambit2/Phrases.java
+++ iuclid_5_5-io/src/main/java/net/idea/i5/_5/ambit2/Phrases.java
@@ -34,7 +34,182 @@ public class Phrases {
 	public static final Hashtable<String, String> phrasegroup_A101 = phrasegroup_A101();
 	public static final Hashtable<String, String> phrasegroup_Z36 = phrasegroup_Z36();
 	public static final Hashtable<String, String> phrasegroup_A03 = phrasegroup_A03();
+	public static final Hashtable<String, String> phrasegroup_E02 = phrasegroup_E02();
 	
+	  
+    
+    private static Hashtable<String, String> phrasegroup_E02() {
+    	Hashtable<String, String> p = new Hashtable<String,String>();
+
+	      p.put("55"  ,"Alburnus albidus costa - ");
+	      p.put("56","Alburnus alburnus - ");
+	      p.put("57","Alburnus lucidus - ");
+	      p.put("58","Alburnus sp. - ");
+	      p.put("65","Alosa pseudobarengus - ");
+	      p.put("87","Anchoa mitchilli - ");
+	      p.put("90","Anguilla anguilla - ");
+	      p.put("91","Anguilla japonica - ");
+	      p.put("92","Anguilla rostrata - ");
+	      p.put("93","Anguilla sp. - ");
+	      p.put("140","Barbus barbus - ");
+	      p.put("4455","Brachydanio rerio (new name: Danio rerio) - ");
+	      p.put("161","Brevoortia patronus - ");
+	      p.put("162","Brevoortia tyrannus - ");
+	      p.put("213","Carassius auratus - ");
+	      p.put("214","Carassius carassius - ");
+	      p.put("215","Carassius sp. - ");
+	      p.put("216","Carassius vulgaris - ");
+	      p.put("231","Catostomus commersoni - ");
+	      p.put("233","Centropomus undecimalis - ");
+	      p.put("234","Centropristis striata - ");
+	      p.put("259","Clupea harengus - ");
+	      p.put("264","Colisa fasciatus - ");
+	      p.put("274","Coregonus artedii - ");
+	      p.put("275","Coregonus clupeaformis - ");
+	      p.put("290","Ctenopharyngodon idella - ");
+	      p.put("296","Cymatogaster aggregata - ");
+	      p.put("297","Cynoscion nebulosus - ");
+	      p.put("298","Cyprinodon sp. - ");
+	      p.put("299","Cyprinodon variegatus - ");
+	      p.put("300","Cyprinus auratus - ");
+	      p.put("301","Cyprinus carassius - ");
+	      p.put("302","Cyprinus carpio - ");
+	      p.put("303","Cyprinus sp. - ");
+	      p.put("324","Danio rerio - ");
+	      p.put("3892","Danio rerio (reported as Brachydanio rerio) - ");
+	      p.put("341","Dorosoma petenese - ");
+	      p.put("750","Esox lucius - ");
+	      p.put("751","Esox masquinongi - ");
+	      p.put("752","Esox niger - ");
+	      p.put("753","Esox sp. - ");
+	      p.put("781","Fundulus confluentus - ");
+	      p.put("782","Fundulus diaphanus - ");
+	      p.put("783","Fundulus grandis - ");
+	      p.put("784","Fundulus heteroclitus - ");
+	      p.put("785","Fundulus jenkinsi - ");
+	      p.put("786","Fundulus lucidae - ");
+	      p.put("787","Fundulus majalis - ");
+	      p.put("788","Fundulus similis - ");
+	      p.put("789","Fundulus sp. - ");
+	      p.put("794","Gadus morrhua - ");
+	      p.put("795","Gambusia affinis - ");
+	      p.put("803","Gasterosteus aculeatus - ");
+	      p.put("806","Gaus mexlaughs - ");
+	      p.put("830","Harengula pensacolae - ");
+	      p.put("883","Ictalurus catus - ");
+	      p.put("884","Ictalurus furcatus - ");
+	      p.put("885","Ictalurus melas - ");
+	      p.put("886","Ictalurus natalis - ");
+	      p.put("887","Ictalurus nebulosus - ");
+	      p.put("888","Ictalurus punctatus - ");
+	      p.put("889","Ictalurus sp. - ");
+	      p.put("891","Idus idus - ");
+	      p.put("920","Jordanella floridae - ");
+	      p.put("984","Lagodon rhomboides - ");
+	      p.put("986","Lebistes reticulatus - ");
+	      p.put("988","Leiostomus xanthurus - ");
+	      p.put("999","Lepomis auritus - ");
+	      p.put("1000","Lepomis cyanellus - ");
+	      p.put("1001","Lepomis gibbosus - ");
+	      p.put("1002","Lepomis humilis - ");
+	      p.put("1003","Lepomis macrochirus - ");
+	      p.put("1004","Lepomis microlophus - ");
+	      p.put("1005","Lepomis pallidus - ");
+	      p.put("1006","Lepomis sp. - ");
+	      p.put("1008","Leuciscus cephalus cabeda rissa - ");
+	      p.put("1009","Leuciscus idus - ");
+	      p.put("1010","Leuciscus idus melanotus - ");
+	      p.put("1011","Leuciscus rutilus - ");
+	      p.put("1012","Leuciscus sp. - ");
+	      p.put("1015","Limanda aspera - ");
+	      p.put("1016","Limanda limanda - ");
+	      p.put("1017","Limanda sp. - ");
+	      p.put("1062","Menidia beryllina - ");
+	      p.put("1063","Menidia menidia - ");
+	      p.put("1064","Menidia peninsulae - ");
+	      p.put("1065","Menidia sp. - ");
+	      p.put("1071","Micropogon undulatus - ");
+	      p.put("1072","Micropterus dolomieui - ");
+	      p.put("1073","Micropterus salmoides - ");
+	      p.put("1074","Micropterus sp. - ");
+	      p.put("1079","Misgurnus anguillicaudatus - ");
+	      p.put("1087","Morone chrysops - ");
+	      p.put("1088","Morone saxatilis - ");
+	      p.put("1089","Morone sp. - ");
+	      p.put("1093","Mugil cephalus - ");
+	      p.put("1094","Mugil curema - ");
+	      p.put("1095","Mugil sp. - ");
+	      p.put("1178","Notropis atherinoides - ");
+	      p.put("1316","Oncorhynchus gorbuscha - ");
+	      p.put("1317","Oncorhynchus keta - ");
+	      p.put("1318","Oncorhynchus kisutch - ");
+	      p.put("1319","Oncorhynchus mykiss - ");
+	      p.put("3893","Oncorhynchus mykiss (reported as Salmo gairdneri) - ");
+	      p.put("1321","Oncorhynchus nerka - ");
+	      p.put("1322","Oncorhynchus nerka kennerlyi - ");
+	      p.put("1323","Oncorhynchus sp. - ");
+	      p.put("1324","Oncorhynchus tschawytscha - ");
+	      p.put("1337","Oryzias latipes - ");
+	      p.put("1340","Osmerus mordax - ");
+	      p.put("1357","Parophrys vetulus - ");
+	      p.put("1359","Perca flavescens - ");
+	      p.put("1360","Perca fluviatilis - ");
+	      p.put("1361","Perca sp. - ");
+	      p.put("1364","Petromyzon fluviatilis - ");
+	      p.put("1365","Petromyzon marinus - ");
+	      p.put("1366","Petromyzon sp. - ");
+	      p.put("1373","Phoxinus laevis - ");
+	      p.put("1374","Phoxinus phoxinus - ");
+	      p.put("1375","Phoxinus sp. - ");
+	      p.put("1377","Pimephales notatus - ");
+	      p.put("1378","Pimephales promelas - ");
+	      p.put("1379","Pimephales sp. - ");
+	      p.put("1383","Platypoecilus maculatus - ");
+	      p.put("1386","Pleuronectes platessa - ");
+	      p.put("1388","Poecilia latipinna - ");
+	      p.put("1389","Poecilia reticulata - ");
+	      p.put("1390","Poecilia sp. - ");
+	      p.put("1393","Pogonias cromis - ");
+	      p.put("1396","Pomatomus saltatrix - ");
+	      p.put("1397","Pomoxis annularis - ");
+	      p.put("1398","Pomoxis nigromaculatus - ");
+	      p.put("1430","Prosopium williamsoni - ");
+	      p.put("1439","Pseudopleuronectes americanus - ");
+	      p.put("1442","Ptychocheilus oregonensis - ");
+	      p.put("1477","Rasbora heteromorpha - ");
+	      p.put("1490","Rhodeus sericeus - ");
+	      p.put("1494","Roccus americanus - ");
+	      p.put("1498","Rutilus rutilus - ");
+	      p.put("1510","Salmo aquabonita - ");
+	      p.put("1511","Salmo clarki - ");
+	      p.put("1320","Salmo gairdneri (new name: Oncorhynchus mykiss) - ");
+	      p.put("1512","Salmo irideus - ");
+	      p.put("1513","Salmo salar - ");
+	      p.put("1514","Salmo sp. - ");
+	      p.put("1515","Salmo trutta - ");
+	      p.put("1516","Salvelinus alpinus - ");
+	      p.put("1517","Salvelinus fontinalis - ");
+	      p.put("1518","Salvelinus malma - ");
+	      p.put("1519","Salvelinus namaycush - ");
+	      p.put("1520","Salvelinus sp. - ");
+	      p.put("1526","Sardinops sagax - ");
+	      p.put("1527","Sarotherodon mossambicus - ");
+	      p.put("1531","Scardinius erythrophthalmus - ");
+	      p.put("1537","Sciaenops ocellata - ");
+	      p.put("1545","Semolitus atromaculatus - ");
+	      p.put("1569","Sphaeroidus maculatus - ");
+	      p.put("1581","Stizostedion canadense - ");
+	      p.put("1582","Stizostedion v. vitreum - ");
+	      p.put("1611","Tinca sp. - ");
+	      p.put("1612","Tinca tinca - ");
+	      p.put("1613","Tinca vulgaris - ");
+	      p.put("1639","Trutta iridea - ");
+	      p.put("1342","other: - ");
+	      p.put("1173","no data - ");
+	      p.put("NOT_SPECIFIED","");
+
+    	return p;
+    }
 	private static Hashtable<String, String> phrasegroup_A03() {
     	Hashtable<String, String> p = new Hashtable<String,String>();
     	p.put("2480", "yes - ");
@@ -265,6 +440,18 @@ public class Phrases {
     	return p;
     }
     
+    private static Hashtable<String, String> phrasegroup_N24() {
+    	Hashtable<String, String> p = new Hashtable<String,String>();
+    	p.put("2505", "% (w/w)");
+    	p.put("2506", "% (v/v)");
+    	p.put("2098", "mg/L");
+    	p.put("3437", "mg/kg");
+    	p.put("2283", "ppm");
+    	p.put("3440", ">mg/m\u00B3");
+    	p.put("NOT_SPECIFIED", ""); 
+    	return p;
+    }
+    
     private static Hashtable<String, String> initPhrasegroup_N28() {
     	Hashtable<String, String> p = new Hashtable<String,String>();
     	p.put("2675", "absorbent");
@@ -307,18 +494,7 @@ public class Phrases {
     	p.put("NOT_SPECIFIED", ""); 
     	return p;
     }	    
-    
-    private static Hashtable<String, String> phrasegroup_N24() {
-    	Hashtable<String, String> p = new Hashtable<String,String>();
-    	p.put("2505", "% (w/w)");
-    	p.put("2506", "% (v/v)");
-    	p.put("2098", "mg/L");
-    	p.put("3437", "mg/kg");
-    	p.put("2283", "ppm");
-    	p.put("3440", ">mg/m\u00B3");
-    	p.put("NOT_SPECIFIED", ""); 
-    	return p;
-    }
+
 
     private static Hashtable<String, String> phrasegroup_A02() {
     	Hashtable<String, String> p = new Hashtable<String,String>();
