23a022d627375314b24e2bbc6b50b6f8bb3cdb9e
ngn <jeliazkova.nina@gmail.com>
2014-05-21 13:04:22 +0300

    more tests

--- iuclid_5-ws/src/test/java/net/idea/i5/cli/test/QueryToolClientTest.java
+++ iuclid_5-ws/src/test/java/net/idea/i5/cli/test/QueryToolClientTest.java
@@ -2,6 +2,7 @@ package net.idea.i5.cli.test;
 
 import java.io.File;
 import java.util.List;
+import java.util.logging.Level;
 
 import junit.framework.Assert;
 import net.idea.i5.cli.Container;
@@ -17,7 +18,7 @@ import org.junit.Test;
 public class QueryToolClientTest  extends ClientTest {
 	
 	@Test
-	public void test() throws Exception {
+	public void testUUID() throws Exception {
 		long now = System.currentTimeMillis();
 		try {
 			if (i5.login(PropertiesUtil.getUsername(),PropertiesUtil.getPassword())) {
@@ -41,6 +42,78 @@ public class QueryToolClientTest  extends ClientTest {
 			try {i5.logout();} catch (Exception x) {}
 			System.out.println(System.currentTimeMillis()-now);
 		}
+	}
+	
+	@Test
+	public void testCompTox() throws Exception {
+		long now = System.currentTimeMillis();
+		try {
+			if (i5.login(PropertiesUtil.getUsername(),PropertiesUtil.getPassword())) {
+				int expected = 3;
+				QueryToolClient cli = i5.getQueryToolClient();
+								
+				String[] identifier = {"IUC5-a438545b-3c6c-403f-83e6-5cd2756d77d6/0",
+									   "IUC5-eb8957ab-538b-48b6-a024-e9875fbd48b1/0",
+									   "IUC5-541ccdba-0033-45ee-8136-406478deb0f4/0",
+									   "IUC5-203e228e-7422-4f76-9068-82033f998d88/0"
+									   };
+				List<IIdentifiableResource<String>> content = cli.executeQuery(PredefinedQuery.query_by_it_identifier,"CompTox","Ambit Transfer1");
+				Assert.assertNotNull(content);
+				for (int j=0; j < content.size(); j++) {
+					System.out.println(content.get(j).getResourceIdentifier());
+				}
+				Assert.assertEquals(expected,content.size());
+				int i = 0;
+				
+				for (int j=0; j < content.size(); j++) {
+					boolean found = false;
+					for (String id : identifier) 
+						if (id.equals(content.get(j).getResourceIdentifier())) { found = true; break;}
+					if (!found)
+						System.err.println(content.get(j).getResourceIdentifier());
+					else 
+						i++;
+				}	
+				Assert.assertEquals(expected,i);
+				
+				for (int j=0; j < content.size();  j++) {
+					Assert.assertTrue(content.get(j) instanceof Container);
+					ContainerClient ccli = i5.getContainerClient();
+					try {
+						getCompToxContainer(ccli, content.get(j).getResourceIdentifier());
+					} catch (Exception x) {
+						logger.log(Level.SEVERE,content.get(j).getResourceIdentifier(),x);
+					}
+				}	
+			}
+		} catch (Exception x) {
+			throw x;
+		} finally {
+			try {i5.logout();} catch (Exception x) {}
+			try {i5.getHttpClient().getConnectionManager().shutdown();} catch (Exception x) {}
+			System.out.println(System.currentTimeMillis()-now);
+		}
+	}
+	
+	protected void getCompToxContainer(ContainerClient cli,String identifier) throws Exception {
+		File file = null;
+		List<IIdentifiableResource<String>> content = cli.get(identifier);
+		Assert.assertNotNull(content);
+		System.out.println(identifier);
+		System.out.println(content.size());
+		Assert.assertNotNull(content);
+//		Assert.assertEquals(1,content.size());
+		//Assert.assertEquals(identifier,content.get(0).getResourceIdentifier());
+		//Assert.assertTrue(content.get(0) instanceof Container);
+		
+		for (int i=0; i < content.size();i++) {
+			file = ((Container)content.get(i)).getIpzarchive();
+			Assert.assertTrue(file.exists());
+			//Assert.assertTrue(file.length()>0);
+			System.out.println(file);
+			//unmarshall_i5z(file,-1);
+		}
+		
 	}
 	
 	protected void getContainer(ContainerClient cli,String identifier) throws Exception {
