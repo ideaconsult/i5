9b877f0ab2c4813bb451691398adf5226b6eb5c8
ngn <jeliazkova.nina@gmail.com>
2014-08-11 16:47:12 +0300

    doses moved as parameter to dose response (naturally)

--- iuclid_5_common/src/main/java/net/idea/i5/io/I5_ROOT_OBJECTS.java
+++ iuclid_5_common/src/main/java/net/idea/i5/io/I5_ROOT_OBJECTS.java
@@ -1639,7 +1639,7 @@ public enum I5_ROOT_OBJECTS {
 		@Override
 		public String[] getConditions() {
 			return new String[] {
-					I5CONSTANTS.cDoses,"Replicate","Emission wavelength"
+					"Replicate","Emission wavelength"
 			};
 		}
 	},
@@ -1666,6 +1666,12 @@ public enum I5_ROOT_OBJECTS {
 		@Override
 		public String getTopCategory() {
 			return "TOX";
+		}
+		@Override
+		public String[] getConditions() {
+			return new String[] {
+					I5CONSTANTS.cDoses,"Replicate","Emission wavelength"
+			};
 		}
 	},		
 	PUBCHEM_PANEL {
