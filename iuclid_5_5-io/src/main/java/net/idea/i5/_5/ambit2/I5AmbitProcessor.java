400302f910a8473ed82f904681ed7c5b62ae9415
vedina <jeliazkova.nina@gmail.com>
2014-12-28 20:40:09 +0200

    QA criteriat logging level changed to FINE

--- iuclid_5_5-io/src/main/java/net/idea/i5/_5/ambit2/I5AmbitProcessor.java
+++ iuclid_5_5-io/src/main/java/net/idea/i5/_5/ambit2/I5AmbitProcessor.java
@@ -74,7 +74,7 @@ public class I5AmbitProcessor<Target> extends DefaultAmbitProcessor<Target, IStr
 				return convertor.transform2record(unmarshalled, record);
 		} catch (QACriteriaException x) { 
 			//reliability exception
-			logger.log(Level.INFO,x.getMessage());
+			logger.log(Level.FINE,x.getMessage());
 			return null;
 		} catch (AmbitException x) {
 			throw x;
