42eb20d7851545d23dcbdda2a71762b6d036cb85
ngn <jeliazkova.nina@gmail.com>
2017-02-28 10:18:09 +0200

    update using ontology lookup

--- demo/init.js
+++ demo/init.js
@@ -4,36 +4,8 @@ $.ajax({
   dataType: "json",
   jsonpCallback: "callback",
   success: function(data) {
-    var facets = data.facet;
-    var topgroup = {
-    };
-    $.each(data.facet,function(index,facet) {
-      var group =  {
-        label: facet.value,
-        weight: facet.count,
-        groups : [
-        	{label : "Substances", weight : facet.substancescount },
-        	{label : "Measurements", weight : facet.count}
-        ],
-        uri : facet.uri
-      };    	
-    	var subcategory = topgroup[facet.subcategory];
-    	if (subcategory === undefined) {
-    		topgroup[facet.subcategory] = 
-        	{label : facet.subcategory, 
-        	 weight : facet.count, groups: [group],
-        	 uri : facet.subcategoryuri,
-        	 exposed : true
-        	};
-      } else {
-      	   subcategory.weight += facet.count;	
-      	   subcategory.groups.push(group);
-      };
-    });
-		var groups = [];
-		$.each(topgroup,function (index,element) {
-				groups.push(element);
-	  });	
+
+		var groups = mapfacet(data.facet);
 		
     foamtree.set({
       dataObject: { groups: groups },
@@ -47,4 +19,48 @@ $.ajax({
     		});
   }
 });
+}
+
+function initlocal(foamtree, data) {
+	
+		var groups = mapfacet(data);
+		
+    foamtree.set({
+      dataObject: { groups: groups },
+      rolloutDuration: 3000
+    });
+}
+
+function mapfacet(data) {
+	var topgroup = {  };
+
+	$.each(data,function(index,facet) {
+      var group =  {
+        label: facet.value,
+        weight: facet.count,
+        groups : [
+        	{label : "Substances", weight : facet.substancescount },
+        	{label : "Measurements", weight : facet.count}
+        ],
+        uri : facet.uri
+      };    	
+    	var subcategory = topgroup[facet.subcategory];
+    	var nmtypelabel = lookup[facet.subcategory]
+    	if (subcategory === undefined) {
+    		topgroup[facet.subcategory] = 
+        	{label : nmtypelabel===undefined?facet.subcategory:nmtypelabel, 
+        	 weight : facet.count, groups: [group],
+        	 uri : facet.subcategoryuri,
+        	 exposed : true
+        	};
+      } else {
+      	   subcategory.weight += facet.count;	
+      	   subcategory.groups.push(group);
+      };
+    });
+		var groups = [];
+		$.each(topgroup,function (index,element) {
+				groups.push(element);
+	  });	
+	  return groups;
 }
