e8273595cbe006ed9ff155e1dfa7294e5efaede6
vedina <jeliazkova.nina@gmail.com>
2017-04-17 21:44:15 +0300

    missing files added

---
+++ iuclid_6_sections/src/main/java/eu/europa/echa/iuclid6/namespaces/endpoint_study_record_toxicityreproduction/_2/ObjectFactory.java
@@ -0,0 +1,624 @@
+
+package eu.europa.echa.iuclid6.namespaces.endpoint_study_record_toxicityreproduction._2;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the eu.europa.echa.iuclid6.namespaces.endpoint_study_record_toxicityreproduction._2 package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: eu.europa.echa.iuclid6.namespaces.endpoint_study_record_toxicityreproduction._2
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ENDPOINTSTUDYRECORDToxicityReproduction }
+     * 
+     */
+    public ENDPOINTSTUDYRECORDToxicityReproduction createENDPOINTSTUDYRECORDToxicityReproduction() {
+        return new ENDPOINTSTUDYRECORDToxicityReproduction();
+    }
+
+    /**
+     * Create an instance of {@link AdministrativeData }
+     * 
+     */
+    public AdministrativeData createAdministrativeData() {
+        return new AdministrativeData();
+    }
+
+    /**
+     * Create an instance of {@link DataSource }
+     * 
+     */
+    public DataSource createDataSource() {
+        return new DataSource();
+    }
+
+    /**
+     * Create an instance of {@link MaterialsAndMethods }
+     * 
+     */
+    public MaterialsAndMethods createMaterialsAndMethods() {
+        return new MaterialsAndMethods();
+    }
+
+    /**
+     * Create an instance of {@link ResultsAndDiscussion }
+     * 
+     */
+    public ResultsAndDiscussion createResultsAndDiscussion() {
+        return new ResultsAndDiscussion();
+    }
+
+    /**
+     * Create an instance of {@link OverallRemarksAttachments }
+     * 
+     */
+    public OverallRemarksAttachments createOverallRemarksAttachments() {
+        return new OverallRemarksAttachments();
+    }
+
+    /**
+     * Create an instance of {@link ApplicantSummaryAndConclusion }
+     * 
+     */
+    public ApplicantSummaryAndConclusion createApplicantSummaryAndConclusion() {
+        return new ApplicantSummaryAndConclusion();
+    }
+
+    /**
+     * Create an instance of {@link AttachedBackgroundMaterial }
+     * 
+     */
+    public AttachedBackgroundMaterial createAttachedBackgroundMaterial() {
+        return new AttachedBackgroundMaterial();
+    }
+
+    /**
+     * Create an instance of {@link AttachedBackgroundMaterialEntry }
+     * 
+     */
+    public AttachedBackgroundMaterialEntry createAttachedBackgroundMaterialEntry() {
+        return new AttachedBackgroundMaterialEntry();
+    }
+
+    /**
+     * Create an instance of {@link ResultsOfExaminationsParentalAnimals }
+     * 
+     */
+    public ResultsOfExaminationsParentalAnimals createResultsOfExaminationsParentalAnimals() {
+        return new ResultsOfExaminationsParentalAnimals();
+    }
+
+    /**
+     * Create an instance of {@link ResultsP1SecondParentalGeneration }
+     * 
+     */
+    public ResultsP1SecondParentalGeneration createResultsP1SecondParentalGeneration() {
+        return new ResultsP1SecondParentalGeneration();
+    }
+
+    /**
+     * Create an instance of {@link ResultsOfExaminationsOffspring }
+     * 
+     */
+    public ResultsOfExaminationsOffspring createResultsOfExaminationsOffspring() {
+        return new ResultsOfExaminationsOffspring();
+    }
+
+    /**
+     * Create an instance of {@link ResultsF2Generation }
+     * 
+     */
+    public ResultsF2Generation createResultsF2Generation() {
+        return new ResultsF2Generation();
+    }
+
+    /**
+     * Create an instance of {@link ReproductiveToxicity }
+     * 
+     */
+    public ReproductiveToxicity createReproductiveToxicity() {
+        return new ReproductiveToxicity();
+    }
+
+    /**
+     * Create an instance of {@link AnyOtherInformationOnResultsInclTables }
+     * 
+     */
+    public AnyOtherInformationOnResultsInclTables createAnyOtherInformationOnResultsInclTables() {
+        return new AnyOtherInformationOnResultsInclTables();
+    }
+
+    /**
+     * Create an instance of {@link ReproductiveToxicity2 }
+     * 
+     */
+    public ReproductiveToxicity2 createReproductiveToxicity2() {
+        return new ReproductiveToxicity2();
+    }
+
+    /**
+     * Create an instance of {@link Entry }
+     * 
+     */
+    public Entry createEntry() {
+        return new Entry();
+    }
+
+    /**
+     * Create an instance of {@link LowestEffectiveDoseConc }
+     * 
+     */
+    public LowestEffectiveDoseConc createLowestEffectiveDoseConc() {
+        return new LowestEffectiveDoseConc();
+    }
+
+    /**
+     * Create an instance of {@link GeneralToxicityF2 }
+     * 
+     */
+    public GeneralToxicityF2 createGeneralToxicityF2() {
+        return new GeneralToxicityF2();
+    }
+
+    /**
+     * Create an instance of {@link DevelopmentalNeurotoxicityOfF1Generation }
+     * 
+     */
+    public DevelopmentalNeurotoxicityOfF1Generation createDevelopmentalNeurotoxicityOfF1Generation() {
+        return new DevelopmentalNeurotoxicityOfF1Generation();
+    }
+
+    /**
+     * Create an instance of {@link DevelopmentalImmunotoxicityOfF1Generation }
+     * 
+     */
+    public DevelopmentalImmunotoxicityOfF1Generation createDevelopmentalImmunotoxicityOfF1Generation() {
+        return new DevelopmentalImmunotoxicityOfF1Generation();
+    }
+
+    /**
+     * Create an instance of {@link DetailsOnResultsF2 }
+     * 
+     */
+    public DetailsOnResultsF2 createDetailsOnResultsF2() {
+        return new DetailsOnResultsF2();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsF2 }
+     * 
+     */
+    public EffectLevelsF2 createEffectLevelsF2() {
+        return new EffectLevelsF2();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityF2 }
+     * 
+     */
+    public TargetSystemOrganToxicityF2 createTargetSystemOrganToxicityF2() {
+        return new TargetSystemOrganToxicityF2();
+    }
+
+    /**
+     * Create an instance of {@link F2TargetSystemOrganToxicity }
+     * 
+     */
+    public F2TargetSystemOrganToxicity createF2TargetSystemOrganToxicity() {
+        return new F2TargetSystemOrganToxicity();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityF2Entry }
+     * 
+     */
+    public TargetSystemOrganToxicityF2Entry createTargetSystemOrganToxicityF2Entry() {
+        return new TargetSystemOrganToxicityF2Entry();
+    }
+
+    /**
+     * Create an instance of {@link F2LowestEffectiveDoseConc }
+     * 
+     */
+    public F2LowestEffectiveDoseConc createF2LowestEffectiveDoseConc() {
+        return new F2LowestEffectiveDoseConc();
+    }
+
+    /**
+     * Create an instance of {@link F2Efflevel }
+     * 
+     */
+    public F2Efflevel createF2Efflevel() {
+        return new F2Efflevel();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsF2Entry }
+     * 
+     */
+    public EffectLevelsF2Entry createEffectLevelsF2Entry() {
+        return new EffectLevelsF2Entry();
+    }
+
+    /**
+     * Create an instance of {@link GeneralToxicityF1 }
+     * 
+     */
+    public GeneralToxicityF1 createGeneralToxicityF1() {
+        return new GeneralToxicityF1();
+    }
+
+    /**
+     * Create an instance of {@link DevelopmentalNeurotoxicityF1 }
+     * 
+     */
+    public DevelopmentalNeurotoxicityF1 createDevelopmentalNeurotoxicityF1() {
+        return new DevelopmentalNeurotoxicityF1();
+    }
+
+    /**
+     * Create an instance of {@link DevelopmentalImmunotoxicityF1 }
+     * 
+     */
+    public DevelopmentalImmunotoxicityF1 createDevelopmentalImmunotoxicityF1() {
+        return new DevelopmentalImmunotoxicityF1();
+    }
+
+    /**
+     * Create an instance of {@link DetailsOnResultsF1 }
+     * 
+     */
+    public DetailsOnResultsF1 createDetailsOnResultsF1() {
+        return new DetailsOnResultsF1();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsF1 }
+     * 
+     */
+    public EffectLevelsF1 createEffectLevelsF1() {
+        return new EffectLevelsF1();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityF1 }
+     * 
+     */
+    public TargetSystemOrganToxicityF1 createTargetSystemOrganToxicityF1() {
+        return new TargetSystemOrganToxicityF1();
+    }
+
+    /**
+     * Create an instance of {@link F1TargetSystemOrganToxicity }
+     * 
+     */
+    public F1TargetSystemOrganToxicity createF1TargetSystemOrganToxicity() {
+        return new F1TargetSystemOrganToxicity();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityF1Entry }
+     * 
+     */
+    public TargetSystemOrganToxicityF1Entry createTargetSystemOrganToxicityF1Entry() {
+        return new TargetSystemOrganToxicityF1Entry();
+    }
+
+    /**
+     * Create an instance of {@link F1LowestEffectiveDoseConc }
+     * 
+     */
+    public F1LowestEffectiveDoseConc createF1LowestEffectiveDoseConc() {
+        return new F1LowestEffectiveDoseConc();
+    }
+
+    /**
+     * Create an instance of {@link F1Efflevel }
+     * 
+     */
+    public F1Efflevel createF1Efflevel() {
+        return new F1Efflevel();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsF1Entry }
+     * 
+     */
+    public EffectLevelsF1Entry createEffectLevelsF1Entry() {
+        return new EffectLevelsF1Entry();
+    }
+
+    /**
+     * Create an instance of {@link GeneralToxicityP1 }
+     * 
+     */
+    public GeneralToxicityP1 createGeneralToxicityP1() {
+        return new GeneralToxicityP1();
+    }
+
+    /**
+     * Create an instance of {@link ReproductiveFunctionPerformanceP1 }
+     * 
+     */
+    public ReproductiveFunctionPerformanceP1 createReproductiveFunctionPerformanceP1() {
+        return new ReproductiveFunctionPerformanceP1();
+    }
+
+    /**
+     * Create an instance of {@link DetailsOnResultsP1 }
+     * 
+     */
+    public DetailsOnResultsP1 createDetailsOnResultsP1() {
+        return new DetailsOnResultsP1();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsP1 }
+     * 
+     */
+    public EffectLevelsP1 createEffectLevelsP1() {
+        return new EffectLevelsP1();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityP1 }
+     * 
+     */
+    public TargetSystemOrganToxicityP1 createTargetSystemOrganToxicityP1() {
+        return new TargetSystemOrganToxicityP1();
+    }
+
+    /**
+     * Create an instance of {@link P1TargetSystemOrganToxicity }
+     * 
+     */
+    public P1TargetSystemOrganToxicity createP1TargetSystemOrganToxicity() {
+        return new P1TargetSystemOrganToxicity();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityP1Entry }
+     * 
+     */
+    public TargetSystemOrganToxicityP1Entry createTargetSystemOrganToxicityP1Entry() {
+        return new TargetSystemOrganToxicityP1Entry();
+    }
+
+    /**
+     * Create an instance of {@link P1LowestEffectiveDoseConc }
+     * 
+     */
+    public P1LowestEffectiveDoseConc createP1LowestEffectiveDoseConc() {
+        return new P1LowestEffectiveDoseConc();
+    }
+
+    /**
+     * Create an instance of {@link P1Efflevel }
+     * 
+     */
+    public P1Efflevel createP1Efflevel() {
+        return new P1Efflevel();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsP1Entry }
+     * 
+     */
+    public EffectLevelsP1Entry createEffectLevelsP1Entry() {
+        return new EffectLevelsP1Entry();
+    }
+
+    /**
+     * Create an instance of {@link GeneralToxicityP0 }
+     * 
+     */
+    public GeneralToxicityP0 createGeneralToxicityP0() {
+        return new GeneralToxicityP0();
+    }
+
+    /**
+     * Create an instance of {@link ReproductiveFunctionPerformanceP0 }
+     * 
+     */
+    public ReproductiveFunctionPerformanceP0 createReproductiveFunctionPerformanceP0() {
+        return new ReproductiveFunctionPerformanceP0();
+    }
+
+    /**
+     * Create an instance of {@link DetailsOnResultsP0 }
+     * 
+     */
+    public DetailsOnResultsP0 createDetailsOnResultsP0() {
+        return new DetailsOnResultsP0();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsP0 }
+     * 
+     */
+    public EffectLevelsP0 createEffectLevelsP0() {
+        return new EffectLevelsP0();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityP0 }
+     * 
+     */
+    public TargetSystemOrganToxicityP0 createTargetSystemOrganToxicityP0() {
+        return new TargetSystemOrganToxicityP0();
+    }
+
+    /**
+     * Create an instance of {@link P0TargetSystemOrganToxicity }
+     * 
+     */
+    public P0TargetSystemOrganToxicity createP0TargetSystemOrganToxicity() {
+        return new P0TargetSystemOrganToxicity();
+    }
+
+    /**
+     * Create an instance of {@link TargetSystemOrganToxicityP0Entry }
+     * 
+     */
+    public TargetSystemOrganToxicityP0Entry createTargetSystemOrganToxicityP0Entry() {
+        return new TargetSystemOrganToxicityP0Entry();
+    }
+
+    /**
+     * Create an instance of {@link P0LowestEffectiveDoseConc }
+     * 
+     */
+    public P0LowestEffectiveDoseConc createP0LowestEffectiveDoseConc() {
+        return new P0LowestEffectiveDoseConc();
+    }
+
+    /**
+     * Create an instance of {@link P0Efflevel }
+     * 
+     */
+    public P0Efflevel createP0Efflevel() {
+        return new P0Efflevel();
+    }
+
+    /**
+     * Create an instance of {@link EffectLevelsP0Entry }
+     * 
+     */
+    public EffectLevelsP0Entry createEffectLevelsP0Entry() {
+        return new EffectLevelsP0Entry();
+    }
+
+    /**
+     * Create an instance of {@link Guideline }
+     * 
+     */
+    public Guideline createGuideline() {
+        return new Guideline();
+    }
+
+    /**
+     * Create an instance of {@link TestMaterials }
+     * 
+     */
+    public TestMaterials createTestMaterials() {
+        return new TestMaterials();
+    }
+
+    /**
+     * Create an instance of {@link TestAnimals }
+     * 
+     */
+    public TestAnimals createTestAnimals() {
+        return new TestAnimals();
+    }
+
+    /**
+     * Create an instance of {@link AdministrationExposure }
+     * 
+     */
+    public AdministrationExposure createAdministrationExposure() {
+        return new AdministrationExposure();
+    }
+
+    /**
+     * Create an instance of {@link Examinations }
+     * 
+     */
+    public Examinations createExaminations() {
+        return new Examinations();
+    }
+
+    /**
+     * Create an instance of {@link AnyOtherInformationOnMaterialsAndMethodsInclTables }
+     * 
+     */
+    public AnyOtherInformationOnMaterialsAndMethodsInclTables createAnyOtherInformationOnMaterialsAndMethodsInclTables() {
+        return new AnyOtherInformationOnMaterialsAndMethodsInclTables();
+    }
+
+    /**
+     * Create an instance of {@link DosesConcentrations }
+     * 
+     */
+    public DosesConcentrations createDosesConcentrations() {
+        return new DosesConcentrations();
+    }
+
+    /**
+     * Create an instance of {@link DosesConcentrationsEntry }
+     * 
+     */
+    public DosesConcentrationsEntry createDosesConcentrationsEntry() {
+        return new DosesConcentrationsEntry();
+    }
+
+    /**
+     * Create an instance of {@link DoseConc }
+     * 
+     */
+    public DoseConc createDoseConc() {
+        return new DoseConc();
+    }
+
+    /**
+     * Create an instance of {@link GuidelineEntry }
+     * 
+     */
+    public GuidelineEntry createGuidelineEntry() {
+        return new GuidelineEntry();
+    }
+
+    /**
+     * Create an instance of {@link AttachedJustification }
+     * 
+     */
+    public AttachedJustification createAttachedJustification() {
+        return new AttachedJustification();
+    }
+
+    /**
+     * Create an instance of {@link CrossReference }
+     * 
+     */
+    public CrossReference createCrossReference() {
+        return new CrossReference();
+    }
+
+    /**
+     * Create an instance of {@link CrossReferenceEntry }
+     * 
+     */
+    public CrossReferenceEntry createCrossReferenceEntry() {
+        return new CrossReferenceEntry();
+    }
+
+    /**
+     * Create an instance of {@link AttachedJustificationEntry }
+     * 
+     */
+    public AttachedJustificationEntry createAttachedJustificationEntry() {
+        return new AttachedJustificationEntry();
+    }
+
+}
