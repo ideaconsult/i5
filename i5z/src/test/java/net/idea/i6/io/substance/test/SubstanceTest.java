9b32932a62e6bcd9eaa2a214e24138bbf5c4b350
ngn <jeliazkova.nina@gmail.com>
2017-04-20 09:29:52 +0300

    test

--- i5z/src/test/java/net/idea/i6/io/substance/test/SubstanceTest.java
+++ i5z/src/test/java/net/idea/i6/io/substance/test/SubstanceTest.java
@@ -1,12 +1,15 @@
 package net.idea.i6.io.substance.test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
 import org.junit.Test;
 import org.openscience.cdk.io.IChemObjectReaderErrorHandler;
@@ -17,32 +20,55 @@ import ambit2.base.interfaces.IStructureRecord;
 import ambit2.base.relation.composition.CompositionRelation;
 import junit.framework.Assert;
 import net.idea.i5.io.QASettings;
+import net.idea.i6.io.I6DReader;
 import net.idea.i6.io.I6ManifestReader;
 import net.idea.i6.io.I6ZReader;
 
 public class SubstanceTest {
-	protected static Logger logger = Logger.getLogger(SubstanceTest.class
-			.getName());
-	
+	protected static Logger logger = Logger.getLogger(SubstanceTest.class.getName());
+
 	@Test
 	public void readManifest() throws Exception {
-		try (InputStream in = getClass().getClassLoader()
-				.getResourceAsStream("net/idea/i6/_2/substance/manifest.xml")) {
+		File file = new File(getClass().getClassLoader().getResource("net/idea/i6/_2/substance/manifest.xml").getFile());
+		Assert.assertTrue(file.exists());
+		File folder = file.getParentFile();
+		try (InputStream in = new FileInputStream(file)) {
 			I6ManifestReader r = new I6ManifestReader();
 			Document manifest = r.read(in);
-			
-			Map<String,String> file2cjaxbcp = r.parseFiles(manifest, new File("/tmp"));
+
+			Map<String, String> file2cjaxbcp = r.parseFiles(manifest, folder);
 			System.out.println(file2cjaxbcp);
+
+			File test = new File(folder,"IUC4-05eb6a0b-c37d-3a08-a9a3-718494b352ef_0.i6d");
+			Assert.assertEquals("eu.europa.echa.iuclid6.namespaces.endpoint_study_record_watersolubility._2", file2cjaxbcp.get(test.getAbsolutePath()));
+
+			String jaxbcontextpath = "eu.europa.echa.iuclid6.namespaces.platform_container.v1:"
+					+ "eu.europa.echa.iuclid6.namespaces.platform_fields.v1:"
+					+ "eu.europa.echa.iuclid6.namespaces.platform_metadata.v1:" + file2cjaxbcp.get(test.getAbsolutePath());
+
+			JAXBContext jaxbContext = JAXBContext.newInstance(jaxbcontextpath);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			System.out.println(jaxbcontextpath);
+			try (InputStream fileReader = new FileInputStream(test)) {
+				I6DReader reader = new I6DReader(test.getAbsolutePath(), fileReader, jaxbContext, jaxbUnmarshaller, new QASettings(false));
+				while (reader.hasNext()) {
+					Object o = reader.next();
+					Assert.assertNotNull(o);
+				}
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+
 			SubstanceRecord substance = new SubstanceRecord();
 			r.parseDocuments(manifest, substance);
 			Assert.assertNotNull(substance.getSubstanceUUID());
 			Assert.assertNotNull(substance.getContent());
 			Assert.assertEquals("IUC4-efdb21bb-e79f-3286-a988-b6f6944d3734", substance.getSubstanceUUID());
 			Assert.assertNotNull(substance.getReferenceSubstanceUUID());
-			
+
 		}
 	}
-	
+
 	@Test
 	public void test_i6z_2() throws Exception {
 		String test = "net/idea/i6/_2/substance/i6z/IUC4-efdb21bb-e79f-3286-a988-b6f6944d3734.i6z";
@@ -51,7 +77,7 @@ public class SubstanceTest {
 		Assert.assertEquals(603, unmarshall_i6z(new File(url.getFile()), 603));
 		// all 880
 	}
-	
+
 	@Test
 	public void test_i6z_2_echa() throws Exception {
 		String test = "net/idea/i6/_2/substance/i6z/ECHA-13cdf683-927b-47d6-81a5-32d27ec38747.i6z";
@@ -69,7 +95,7 @@ public class SubstanceTest {
 		Assert.assertEquals(38, unmarshall_i6z(new File(url.getFile()), 38));
 		// all 880
 	}
-	
+
 	protected int unmarshall_i6z(File in, int nfiles) throws Exception {
 		Assert.assertNotNull(in);
 		FileOutputStream output = null;
@@ -86,15 +112,15 @@ public class SubstanceTest {
 				}
 
 				public void handleError(String message, int row, int colStart, int colEnd) {
-					//System.out.println(message);
+					// System.out.println(message);
 				}
 
 				public void handleError(String message, Exception exception) {
-					//logger.log(Level.SEVERE, message);
+					// logger.log(Level.SEVERE, message);
 				}
 
 				public void handleError(String message) {
-					//logger.log(Level.SEVERE, message);
+					// logger.log(Level.SEVERE, message);
 				}
 			});
 			int count = 0;
