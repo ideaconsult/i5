3831719376a6fa2b690d03be051b43f5c3d659bd
ngn <jeliazkova.nina@gmail.com>
2017-04-24 07:39:45 +0300

    acture tox wrappers

---
+++ iuclid_6_2-io/src/main/java/net/idea/i6/_2/ambit2/wrappers/AcuteToxicityOral_RecordWrapper.java
@@ -0,0 +1,47 @@
+package net.idea.i6._2.ambit2.wrappers;
+
+import ambit2.base.data.study.EffectRecord;
+import ambit2.base.data.study.IParams;
+import ambit2.base.data.study.ProtocolApplication;
+import eu.europa.echa.iuclid6.namespaces.endpoint_study_record_acutetoxicityoral._2.ENDPOINTSTUDYRECORDAcuteToxicityOral;
+import eu.europa.echa.iuclid6.namespaces.endpoint_study_record_acutetoxicityoral._2.ENDPOINTSTUDYRECORDAcuteToxicityOral.ResultsAndDiscussion.EffectLevels.Entry;
+import eu.europa.echa.iuclid6.namespaces.platform_container.v1.Document;
+import net.idea.i5.io.I5CONSTANTS;
+import net.idea.i6._2.ambit2.EndpointStudyRecordWrapper;
+
+public class AcuteToxicityOral_RecordWrapper extends EndpointStudyRecordWrapper<ENDPOINTSTUDYRECORDAcuteToxicityOral> {
+
+	public AcuteToxicityOral_RecordWrapper(Document doc) throws Exception {
+		super(doc);
+	}
+	
+	@Override
+	protected String dictionaryParams(String key) {
+		if (I5CONSTANTS.cSpecies.equals(key))
+			return key;
+			return super.dictionaryParams(key);
+	}
+
+	@Override
+	public void assignEffectLevels(ProtocolApplication papp, ENDPOINTSTUDYRECORDAcuteToxicityOral studyrecord) {
+		if (studyrecord.getResultsAndDiscussion().getEffectLevels()==null) return;
+		for (Entry e: studyrecord.getResultsAndDiscussion().getEffectLevels().getEntry()) {
+			EffectRecord<String, IParams, String> effect = endpointCategory.createEffectRecord();
+			effect.setEndpoint(p2Value(e.getEndpoint()));
+			q2effectrecord(e.getEffectLevel(), effect);
+			papp.addEffect(effect);
+			
+			effect.getConditions().put(I5CONSTANTS.cSex, p2Value(e.getSex()));
+
+			effect.getConditions().put(I5CONSTANTS.cSpecies,
+					((IParams) papp.getParameters()).get(I5CONSTANTS.cSpecies));
+
+		}
+	}
+	@Override
+	public void assignInterpretationResult(ProtocolApplication papp, ENDPOINTSTUDYRECORDAcuteToxicityOral studyRecord) {
+		papp.setInterpretationResult(p2Value(studyRecord.getApplicantSummaryAndConclusion().getInterpretationOfResults()));
+		papp.setInterpretationCriteria(studyRecord.getApplicantSummaryAndConclusion().getInterpretationOfResults().getRemarks());
+	}
+
+}
